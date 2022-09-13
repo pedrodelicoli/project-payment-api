0465992f7dcfefdc2d5d427313c4079d06995aea
Pedro Delicoli <pedro.delicoli@peakinvest.com.br>
2022-09-13 20:21:34 -0300

    Criando Testes Unitários para a classe Seller.

---
+++ PaymentApi/tests/PaymentApi.Tests/Sellers/SellerTest.cs
@@ -0,0 +1,39 @@
+﻿namespace PaymentApi.Tests.Vendedores
+{
+    public class SellerTest
+    {
+        private readonly Seller expectedSeller;
+        public SellerTest()
+        {
+            expectedSeller = new(1, "376628533809", "Pedro", "pedro@delicoli.com", "18998244525");
+        }
+
+
+        [Fact]
+        public void ShouldCreateSeller()
+        {
+            var seller = new Seller(1, "376628533809", "Pedro", "pedro@delicoli.com", "18998244525");
+
+            seller.Should().NotBeNull();
+            seller.Should().BeEquivalentTo(expectedSeller);
+        }
+    }
+
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Cpf { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Seller(int id, string cpf, string name, string email, string phoneNumber)
+        {
+            Id = id;
+            Cpf = cpf;
+            Name = name;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
