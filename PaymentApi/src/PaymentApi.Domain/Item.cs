d371b9389580e7a75c3d2e3f8a669ccef0f28520
Pedro Delicoli <pedro.delicoli@peakinvest.com.br>
2022-09-13 22:54:13 -0300

    Altera teste da classe Item, adiciona teste de Exceptions

--- PaymentApi/src/PaymentApi.Domain/Item.cs
+++ PaymentApi/src/PaymentApi.Domain/Item.cs
@@ -1,14 +1,20 @@
-﻿namespace PaymentApi.Domain
+﻿using PaymentApi.Domain.Exceptions;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaymentApi.Domain
 {
     public class Item
     {
-        public int Id { get; set; }
+        [Key]
+        public int Id { get; set; }        
         public string Name { get; set; }
         public int Quantity { get; set; }
 
-        public Item(int id, string name, int quantity)
+        public Item(string name, int quantity)
         {
-            Id = id;
+            if (string.IsNullOrEmpty(name)) throw new DomainException("O campo Nome é obrigatório!");
+            if (quantity <= 0) throw new DomainException("O campo Quantidade deve ser maior que zero!");
+
             Name = name;
             Quantity = quantity;
         }
--- PaymentApi/tests/PaymentApi.Tests/Itens/ItemTest.cs
+++ PaymentApi/tests/PaymentApi.Tests/Itens/ItemTest.cs
@@ -5,16 +5,34 @@
         private readonly Item expectedItem;
         public ItemTest()
         {
-            expectedItem = new(1, "Carteira", 2);
+            expectedItem = new("Carteira", 2);
         }
 
 
         [Fact]
         public void ShouldCreateItem()
         {
-            Item item = new(1, "Carteira", 2);
+            Item item = new("Carteira", 2);
 
             item.Should().BeEquivalentTo(expectedItem);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void ShouldThrowExceptions_WhenNameIsInvalid(string invalidName)
+        {
+            Assert.Throws<ArgumentException>(() => new Item(invalidName, 2))
+                .Message.Should().Be("O campo Nome é obrigatório!");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-7)]
+        public void ShouldThrowExceptions_WhenQuantityIsInvalid(int invalidQuantity)
+        {
+            Assert.Throws<ArgumentException>(() => new Item("Carteira", invalidQuantity))
+                .Message.Should().Be("O campo Quantidade deve ser maior que zero!");
         }
     }
     
