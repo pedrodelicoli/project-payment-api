0a2b68da6d47dd6d3a61c848f0d1b1b2201d9c7e
Pedro Delicoli <pedro.delicoli@peakinvest.com.br>
2022-09-13 23:10:44 -0300

    Modifica teste da classe Sale, adiciona teste de Exceptions

--- PaymentApi/src/PaymentApi.Domain/Sale.cs
+++ PaymentApi/src/PaymentApi.Domain/Sale.cs
@@ -1,16 +1,25 @@
-﻿namespace PaymentApi.Domain
+﻿using PaymentApi.Domain.Exceptions;
+using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace PaymentApi.Domain
 {
     public class Sale
     {
+        [Key]
         public int Id { get; set; }
         public DateTime SaleTime { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
         public List<Item> Itens { get; set; }
 
-        public Sale(int id, DateTime saleTime, SaleStatus status, Seller seller, List<Item> itens)
+        public Sale(DateTime saleTime, SaleStatus status, Seller seller, List<Item> itens)
         {
-            Id = id;
+            if (saleTime == new DateTime()) throw new DomainException("O campo Data é obrigatório!");
+            if (seller is null) throw new DomainException("Deve possuir um Vendedor!");
+            if (itens is null || !itens.Any() ) throw new DomainException("Um item deve ser adicionado!");
+
             SaleTime = saleTime;
             Status = status;
             Seller = seller;
--- PaymentApi/tests/PaymentApi.Tests/Sales/SaleTest.cs
+++ PaymentApi/tests/PaymentApi.Tests/Sales/SaleTest.cs
@@ -8,26 +8,67 @@
         private readonly DateTime saleTime = DateTime.Now;
         public SaleTest()
         {
-            expectedSeller = new(1, "376628533809", "Pedro", "pedro@delicoli.com", "18998244525");
+            expectedSeller = new("376628533809", "Pedro", "pedro@delicoli.com", "18998244525");
             expectedItens =  new()
             {
-                new Item(1, "Wallet", 2)
+                new Item("Carteira", 2)
             };
-            expectedSale = new(1, saleTime, SaleStatus.AguardandoPagamento, expectedSeller, expectedItens);
+            expectedSale = new(saleTime, SaleStatus.AguardandoPagamento, expectedSeller, expectedItens);
         }
 
 
         [Fact]
         public void ShouldCreateSale()
         {
-            Seller seller = new(1, "376628533809", "Pedro", "pedro@delicoli.com", "18998244525");
+            Seller seller = new("376628533809", "Pedro", "pedro@delicoli.com", "18998244525");
             List<Item> itens = new()
             {
-                new Item(1, "Wallet", 2)
+                new Item("Carteira", 2)
             };
-            var sale = new Sale(1, saleTime, SaleStatus.AguardandoPagamento, seller, itens);
+            var sale = new Sale(saleTime, SaleStatus.AguardandoPagamento, seller, itens);
 
             sale.Should().BeEquivalentTo(expectedSale);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptions_WhenSaleTimeIsInvalid()
+        {
+            Seller seller = new("376628533809", "Pedro", "pedro@delicoli.com", "18998244525");
+            List<Item> itens = new()
+            {
+                new Item("Carteira", 2)
+            };
+            Assert.Throws<ArgumentException>(() => new Sale(new DateTime(), SaleStatus.AguardandoPagamento, seller, itens))
+                .Message.Should().Be("O campo Data é obrigatório!");
+        }
+
+        [Fact]
+        public void ShouldThrowExceptions_WhenSellerIsInvalid()
+        {
+            List<Item> itens = new()
+            {
+                new Item("Carteira", 2)
+            };
+            Assert.Throws<ArgumentException>(() => new Sale(saleTime, SaleStatus.AguardandoPagamento, null, itens))
+                .Message.Should().Be("Deve possuir um Vendedor!");
+        }
+
+        [Fact]
+        public void ShouldThrowExceptions_WhenItensIsNull()
+        {
+            Seller seller = new("376628533809", "Pedro", "pedro@delicoli.com", "18998244525");
+
+            Assert.Throws<ArgumentException>(() => new Sale(saleTime, SaleStatus.AguardandoPagamento, seller, null))
+                .Message.Should().Be("Um item deve ser adicionado!");
+        }
+
+        [Fact]
+        public void ShouldThrowExceptions_WhenItensIsEmpty()
+        {
+            Seller seller = new("376628533809", "Pedro", "pedro@delicoli.com", "18998244525");
+
+            Assert.Throws<ArgumentException>(() => new Sale(saleTime, SaleStatus.AguardandoPagamento, seller, new List<Item>()))
+                .Message.Should().Be("Um item deve ser adicionado!");
         }
     }   
 }
