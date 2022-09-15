59b06a8e1fb8784569cd71fe4399878960384e9d
Pedro Delicoli <pedro.delicoli@peakinvest.com.br>
2022-09-14 23:01:53 -0300

    Cria projeto de repository e instancia do banco de dados

---
+++ PaymentApi/src/PaymentApi.Repository/DataBaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentApi.Domain;
+
+namespace PaymentApi.Repository
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Itens { get; set; } 
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    modelBuilder.Entity<Sale>().HasMany((s) => s.Itens);
+        //    modelBuilder.Entity<Sale>().HasOne((s) => s.Seller);
+        //}
+        
+    }
+}
--- PaymentApi/src/PaymentApi.Repository/SaleRepository.cs
+++ PaymentApi/src/PaymentApi.Repository/SaleRepository.cs
@@ -1,23 +1,34 @@
-﻿using PaymentApi.Domain;
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentApi.Domain;
 using PaymentApi.Repository.Interfaces;
 
 namespace PaymentApi.Repository
 {
     public class SaleRepository : ISaleRepository
     {
+        private readonly DataBaseContext dataBaseContext;
+        public SaleRepository(DataBaseContext dataBaseContext)
+        {
+            this.dataBaseContext = dataBaseContext;
+        }
         public Sale Create(Sale sale)
         {
-            throw new NotImplementedException();
+            var createdSale = dataBaseContext.Add(sale);
+            dataBaseContext.SaveChanges();
+            return createdSale.Entity;
         }
 
         public Sale GetById(int id)
         {
-            throw new NotImplementedException();
+            return dataBaseContext.Sales.Include((s) => s.Seller)
+                .Include((s) => s.Itens).Where(s => s.Id == id).FirstOrDefault();
         }
 
         public Sale Update(Sale sale)
         {
-            throw new NotImplementedException();
+            var updatedSale = dataBaseContext.Update(sale);
+            dataBaseContext.SaveChanges();
+            return updatedSale.Entity;
         }
     }
 }
